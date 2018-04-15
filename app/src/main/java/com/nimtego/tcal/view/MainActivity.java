38241e284d347c37d5bbd5f6ceaa7d1e83d651f2
nimtego <nimtego@mail.ru>
2018-04-15 23:24:06 +0300

    activity_main

--- app/src/main/java/com/nimtego/tcal/view/MainActivity.java
+++ app/src/main/java/com/nimtego/tcal/view/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements MainView{
     @Override
     public CalculateData getData() {
 /*        CalculateData calculateData = CalculateData.dataBuilder().
-                bill(String.valueOf(findViewById(R.id.billEditText))).tip()*/ // TODO: 12.04.2018  
+                bill(String.valueOf(findViewById(R.id.billEditText))).*/
         return null;
     }
 
